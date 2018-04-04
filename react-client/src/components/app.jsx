698f107200a7b1307e68b1c5e7c402e25adcac63
Matthew Briggs <matthewsimonbriggs@gmail.com>
2018-04-04 13:39:44 +0100

    Started Fresh and went basic. Much Happier now with the setup

---
+++ react-client/src/components/app.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import { browserHistory } from 'react-router';
+import HomePage from './homePage';
+import NavBar from './headerComponent/navBar';
+import Footer from './footerComponent/footer';
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <NavBar />
+          <Route name="home" exact path="/" component={HomePage} />
+          <Footer />
+        </div>
+      </Router>
+    )
+  }
+}
+export default App;
